4d5f146d0f68a4e2b7ec5f321abefbaca3202c07
Ed <iameddywonder@gmail.com>
2024-10-16 08:56:34 -0400

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <Suspense fallback={<div>Loading...</div>}>
           <WebGLBackground />
         </Suspense>
-        <div className="absolute inset-0 flex items-center justify-center p-20 lg:pr-20">
+        <div className="absolute inset-0 flex items-center p-20 lg:pr-20">
             <ChatFeed />
           </div>
         </div>
